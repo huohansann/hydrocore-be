b56b335a4d6dedc13391a3ebdbbc837a19d7b83e
hanyibin <hybwlwl166@163.com>
2025-06-11 18:03:56 +0800

    feat(config): 完善 JSON 数据处理配置
    - 添加未知属性忽略配置，提高系统稳定性
    - 增加日期格式和时区配置，统一时间表示

--- src/main/java/com/siact/config/WebMvcConfig.java
+++ src/main/java/com/siact/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.siact.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
@@ -8,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 
 /**
  * MVC配置
@@ -21,6 +24,12 @@ public class WebMvcConfig {
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
         ObjectMapper mapper = new ObjectMapper();
 
+        // 设置 当请求JSON数据和controller类没有映射的字段，忽略这些未知属性，不抛异常。
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        //日期格式转换
+        mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+        mapper.setTimeZone(TimeZone.getTimeZone("GMT+8"));
 
         //Long、BigDecimal(小数位0丢失)类型转String类型
         SimpleModule simpleModule = new SimpleModule();
