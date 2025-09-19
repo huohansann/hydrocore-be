b546c6138a51c4d9a3a5e2f2c2e1f6bf562ed025
hanyibin <hybwlwl166@163.com>
2025-09-19 16:19:47 +0800

    完善窑炉控制,助燃风控制设定值的风气比 1:助燃风设置entity里新增天然气实例code,同步mysql结构 2:完善助燃风设定值的查询逻辑 3:新增风气比逻辑,风气比=助燃风设定值/天然气设定值

--- src/main/java/com/siact/module/control/dto/ControlSettingWindDTO.java
+++ src/main/java/com/siact/module/control/dto/ControlSettingWindDTO.java
@@ -3,10 +3,12 @@ package com.siact.module.control.dto;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @ApiModel("助燃风控制设定值")
@@ -14,8 +16,11 @@ public class ControlSettingWindDTO {
     @ApiModelProperty("炉号")
     private String number;
 
-    @ApiModelProperty("炉子的孪生dataCode")
-    private String dataCode;
+    @ApiModelProperty("助燃风炉子对应的数字孪生insCode")
+    private String windDataCode;
+
+    @ApiModelProperty("天然气炉子对应的数字孪生insCode")
+    private String gasDataCode;
 
     @ApiModelProperty("风气比dcs值")
     private Double rateDcsVal;
--- src/main/java/com/siact/module/control/entity/ControlSettingWindEntity.java
+++ src/main/java/com/siact/module/control/entity/ControlSettingWindEntity.java
@@ -13,8 +13,10 @@ public class ControlSettingWindEntity {
     private Long id;
     // 炉子编号
     private String number;
-    // 炉子对应的数字孪生编码
-    private String dataCode;
+    // 助燃风炉子对应的数字孪生insCode
+    private String windDataCode;
+    // 天然气炉子对应的数字孪生insCode
+    private String gasDataCode;
     // 炉子下发风气比对应的点位编码
     private String ratePublishCodes;
     // 风气比人工调整值
