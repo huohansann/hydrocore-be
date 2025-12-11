e44b5ca5017da621a8354b7899be16dccecd958a
kzuo <zuokw@siacttech.com>
2025-12-11 11:36:35 +0800

    feat: 调整日志配置和配置保存接口
    调整 legend 配置保存接口
     - 移除保存过期时间, 长期有效
     - 移除配置获取过期时间刷新
    
    新增 logback 日志配置
     - 新增 logback 配置文件, 对日志进行统一配置
     - 新增自定义日志过滤器, 对 SQL 日志进行过滤输出

--- src/main/java/com/siact/module/base/service/impl/FrontendCacheServiceImpl.java
+++ src/main/java/com/siact/module/base/service/impl/FrontendCacheServiceImpl.java
@@ -7,57 +7,40 @@ import com.siact.module.base.service.FrontendCacheService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
+import javax.annotation.Resource;
+import java.util.*;
 
 @Slf4j
 @Service
 public class FrontendCacheServiceImpl implements FrontendCacheService {
-
-    @Value("${frontend.cache.expire:60}")
-    private Long frontendCacheExpire;
-
-    @Autowired
-    private RedisService redisService;
+    private @Resource RedisService redisService;
 
     @Override
     public void setConfig(String userId, String key, String value) {
         log.info("设置配置setConfig userId: {}, key: {}, value: {}", userId, key, value);
-        //  设置key的话  过期时间为10分钟
-        // 为每个人设置hash格式的redis key为userId value为 code - value
+        // 为每个人设置hash格式的 redis key为 userId, value 为 code - value
         HashMap<String, String> paramMap = new HashMap<>();
         paramMap.put(key, value);
         redisService.setCacheMap(getFrontendCache(userId), paramMap);
-
-        redisService.expire(getFrontendCache(userId), frontendCacheExpire, TimeUnit.DAYS);
+        // redisService.expire(getFrontendCache(userId), frontendCacheExpire, TimeUnit.DAYS);
     }
 
     @Override
     public Map<String, String> getConfig(String userId, String keys) {
         log.info("获取配置getConfig userId: {}, keys: {}", userId, keys);
-        if (StringUtils.isBlank(keys)) {
-            return new HashMap<>();
-        }
+        if (StringUtils.isBlank(keys)) return Collections.emptyMap();
 
         String[] keyArr = keys.split(ConstantSymbol.COMMA);
-        // 获取key对应的value
+        // 获取 key 对应的value
         List<Object> cacheValueList = redisService.getMultiCacheMapValue(getFrontendCache(userId), Arrays.asList(keyArr));
-        if (ObjectUtils.isEmpty(cacheValueList)) {
-            return null;
-        }
+        if (ObjectUtils.isEmpty(cacheValueList)) return Collections.emptyMap();
 
         // 刷新过期时间
-        redisService.expire(getFrontendCache(userId), frontendCacheExpire, TimeUnit.DAYS);
+        // redisService.expire(getFrontendCache(userId), frontendCacheExpire, TimeUnit.DAYS);
 
-        // 转换为json字符串返回
+        // 转换为 json 字符串返回
         LinkedHashMap<String, String> resultMap = new LinkedHashMap<>();
         for (int i = 0; i < keyArr.length; i++) {
             Object cacheVal = cacheValueList.get(i);
