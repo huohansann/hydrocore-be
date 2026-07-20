589cbd81be9611b6ab484544bdf212ab4e94ce25
hl <1666323210@qq.com>
2026-07-20 14:26:12 +0800

    清理：移除后端未使用类

--- src/main/java/com/siact/hydrocore/module/base/dto/EnergyFeeModelStyleDTO.java
+++
@@ -1,9 +0,0 @@
-package com.siact.hydrocore.module.base.dto;
-
-import lombok.Data;
-
-@Data
-public class EnergyFeeModelStyleDTO {
-    private String type;
-    private String feeModelStyle;
-}
--- src/main/java/com/siact/hydrocore/module/system/constants/SysConfigCodeConstants.java
+++
@@ -1,8 +0,0 @@
-package com.siact.hydrocore.module.system.constants;
-
-public class SysConfigCodeConstants {
-  public static final String SYSTEM_DISPLAY_NAME = "system_display_name";
-  public static final String SYSTEM_DEFAULT_LOCALE = "system_default_locale";
-  public static final String INTEGRATION_ENDPOINTS = "integration_endpoints";
-  public static final String INTEGRATION_DATACODES = "integration_datacodes";
-}
